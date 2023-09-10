b6bfee92d6d57e1054630f6691d05ecdddb05f1a
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-09-10 17:46:01 +0300

    initial commit

---
+++ week_1/ex1/script.js
@@ -0,0 +1,8 @@
+
+
+window.onload = function init() {
+    var canvas = document.getElementById("gl-canvas");
+    var gl = canvas.getContext('webgl');
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
