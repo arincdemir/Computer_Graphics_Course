19d2c63482ef1363101b5b25b5d19633cb8a3881
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-09-10 19:50:13 +0300

    added color to data

--- week_1/ex3/script.js
+++ week_1/ex3/script.js
@@ -9,9 +9,9 @@ window.onload = function init() {
     gl.viewport(0, 0, canvas.width, canvas.height);
 
     var vertices2 = [
-        vec2(0, 0),
-        vec2(1, 0),
-        vec2(1, 1)
+        vec2(0, 0), vec3(1, 0, 0),
+        vec2(1, 0), vec3(0, 1, 0),
+        vec2(1, 1), vec3(0, 0, 1)
     ];
 
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -21,7 +21,7 @@ window.onload = function init() {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW);
     var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 5 * 4, 0);
     gl.enableVertexAttribArray(vPosition);
     render();
 }
