3fdf083bc82204ce344b4f9856c20e4c1115444b
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-09-10 20:14:10 +0300

    rendering the triangle

--- week_1/ex3/script.js
+++ week_1/ex3/script.js
@@ -8,7 +8,7 @@ window.onload = function init() {
 
     gl.viewport(0, 0, canvas.width, canvas.height);
 
-    var vertices2 = [
+    var vertices = [
         vec2(0, 0), vec3(1, 0, 0),
         vec2(1, 0), vec3(0, 1, 0),
         vec2(1, 1), vec3(0, 0, 1)
@@ -19,10 +19,16 @@ window.onload = function init() {
 
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    // position
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 5 * 4, 0);
     gl.enableVertexAttribArray(vPosition);
+    // color
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 5 * 4, 2 * 4);
+    gl.enableVertexAttribArray(vColor);
+
     render();
 }
 
