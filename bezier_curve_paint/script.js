87d0f4b463ce782ab73011402b94e1d1cde1c3a1
arincdemir <68223368+arincdemir@users.noreply.github.com>
2023-12-15 00:51:58 +0100

    commented the bezier project

--- bezier_curve_paint/script.js
+++ bezier_curve_paint/script.js
@@ -13,6 +13,7 @@ colors = [black, red, yellow, green, cyan, blue, magenta, white]
 
 
 window.onload = function init() {
+    // Get the canvas and the buttons
     var canvas = document.getElementById("gl-canvas");
     var clearButton = document.getElementById("clear-button");
     var pointButton = document.getElementById("point-button");
@@ -22,22 +23,25 @@ window.onload = function init() {
     var clearColorSelector = document.getElementById("clear-color");
     var drawColorSelector = document.getElementById("draw-color");
 
+    // Setup the WebGL context and clear the canvas
     var gl = WebGLUtils.setupWebGL(canvas);
     gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.viewport(0, 0, canvas.width, canvas.height);
 
+    // Define the variables that will be used later
     var maxVerts = 1000000;
-    var index = 0;
-    var numPoints = 0;
+    var index = 0;     // how many vertices have been drawn
     var drawMode = 0; // 0 point, 1 triangle, 2 sphere, 3 bezier
-    var pointSize = 0.02;
-    var bezierPointSize = 0.005;
-    var circleSides = 50
-    var prevPoints = []
-    var prevPointsColors = [];
+    var pointSize = 0.02; // size of the points drawn
+    var bezierPointSize = 0.005; // size of the points drawn for the bezier curve
+    var circleSides = 50 // number of sides of the circle
+    var prevPoints = [] // list that holds the previously clicked points for the current shape
+    var prevPointsColors = []; // list that holds the colors of the previously clicked points for the current shape
 
+
+    // Add event listeners to the buttons
     pointButton.addEventListener("click", function(event) {
         drawMode = 0;
         prevPoints = [];
@@ -62,6 +66,9 @@ window.onload = function init() {
         prevPointsColors = [];
     })
 
+    // Function that draws a point
+    // It places two triangles that form a small square
+    // basically adding two triangles to the vertex buffer
     function drawPoint(x, y, pointSize) {
         var bottomLeft = vec2(x - pointSize, y - pointSize);
         var topLeft = vec2(x - pointSize, y + pointSize);
@@ -78,6 +85,8 @@ window.onload = function init() {
         index += 6;
     }
 
+    // Function that draws a triangle
+    // It places three points in the vertex buffer
     function drawTriangle(x, y) {
         var currentColor = colors[drawColorSelector.selectedIndex];
         prevPoints.push(vec2(x, y));
@@ -92,6 +101,10 @@ window.onload = function init() {
         }
     }
 
+
+    // Function that draws a circle
+    // It calculates the points of the circle using the center and the radius
+    // and then places them in the vertex buffer
     function drawCircle(x, y) {
         prevPoints.push(vec2(x, y));
         var currentColor = colors[drawColorSelector.selectedIndex];
@@ -127,6 +140,13 @@ window.onload = function init() {
         }
     }
 
+    // Function that draws a quadratic bezier curve
+    // It calculates the points of the curve using the three points and t
+    // the function quadraticBezier is defined at the end of the file as a parametric function to calculate the points
+    // we use the function drawPoint to draw the points of the curve
+    // which creates the illusion of a curve
+    // it draws a lot of points though, so it is not very efficient
+    // doing it with lines would be more efficient
     function drawQuadraticBezier(x, y) {
         prevPoints.push(vec2(x, y));
         var currentColor = colors[drawColorSelector.selectedIndex];
@@ -146,6 +166,9 @@ window.onload = function init() {
         }
     }
 
+    // Add event listener to the canvas
+    // When the canvas is clicked, it gets the coordinates of the click
+    // and calls the appropriate function to draw the shape
     canvas.addEventListener("click", function (event) {
         var x = (event.clientX - canvas.width / 2) / (canvas.width / 2);
         var y = 1 - (event.clientY) / (canvas.width / 2);
@@ -169,21 +192,25 @@ window.onload = function init() {
 
     })
 
+    // Add event listener to the clear button
     clearButton.addEventListener("click", function (event) {
         gl.bufferData(gl.ARRAY_BUFFER,  maxVerts * (sizeof["vec2"] + sizeof["vec4"]), gl.STATIC_DRAW);
         index = 0;
-        numPoints = 0;
         var selectedColor = colors[clearColorSelector.selectedIndex];
         gl.clearColor(selectedColor[0], selectedColor[1], selectedColor[2], selectedColor[3]);
         gl.clear(gl.COLOR_BUFFER_BIT);
     })
     
+    // Initialize the shaders and the vertex buffer
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
+    // Create a buffer for the vertex positions and colors
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, maxVerts * (sizeof["vec2"] + sizeof["vec4"]), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
@@ -191,12 +218,13 @@ window.onload = function init() {
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, maxVerts * sizeof["vec2"]);
     gl.enableVertexAttribArray(vColor);
     
+    // Render the scene
     function render() {
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.drawArrays(gl.TRIANGLES, 0, maxVerts);
     }
 
-    
+    // Main loop that calls itself all the time
     function update(){
         render();
         requestAnimationFrame(update);
@@ -205,6 +233,8 @@ window.onload = function init() {
     update();
 }
 
+// Function that calculates the points of a quadratic bezier curve
+// It uses the formula p = (1-t)^2 * p0 + 2 * (1-t) * t * p1 + t^2 * p2
 function quadraticBezier(t, p0, p1, p2) {
     var u = 1 - t;
     var uu = u * u;
